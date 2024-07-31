1d5101a4ca804063a3a5a24ec0928ba474639530
hfmarques <heber.f.marques@gmail.com>
2024-07-31 17:55:18 -0300

    Added consumer

---
+++ src/RabbitMqMessageBus/Consumer/Consumer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMqMessageBus.Publisher;
+
+namespace RabbitMqMessageBus.Consumer;
+
+public interface IConsumer
+{
+    Task ExecuteAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse?>> onMessage, ConsumerParams consumerParams, CancellationToken cancellationToken = default) where TRequest : class where TResponse : class;
+}
+
+public class Consumer(IConnection connection, ILogger<Consumer> logger, IPublisher publisher) : IConsumer
+{
+     public Task ExecuteAsync<TRequest, TResponse>(
+         Func<TRequest, Task<TResponse?>> onMessage,
+         ConsumerParams consumerParams,
+         CancellationToken cancellationToken = default)
+         where TRequest : class where TResponse : class
+     {
+         ArgumentNullException.ThrowIfNull(consumerParams);
+         ArgumentException.ThrowIfNullOrEmpty(consumerParams.Queue);
+
+         return Task.CompletedTask;
+     }
+}
