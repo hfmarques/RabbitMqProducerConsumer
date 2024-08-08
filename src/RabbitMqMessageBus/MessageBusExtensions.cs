8cabb8bbc850f867dbcb29ce37dd9b6a3890f4d3
hfmarques <heber.f.marques@gmail.com>
2024-08-08 13:04:04 -0300

    Added MessageBusExtensions.cs

---
+++ src/RabbitMqMessageBus/MessageBusExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RabbitMqMessageBus.Consumer;
+using RabbitMqMessageBus.Publisher;
+
+namespace RabbitMqMessageBus;
+
+public static class MessageBusExtensions
+{
+    public static void AddServicesFromMessageBus(this IServiceCollection services)
+    {
+        services.AddTransient<IPublisher, Publisher.Publisher>();
+        services.AddTransient<IConsumer, Consumer.Consumer>();
+    }
+}
